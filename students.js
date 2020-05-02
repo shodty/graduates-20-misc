665a3c25b45a295c5ba1b88eea709bd73e39b6a4
shodty <32799489+shodty@users.noreply.github.com>
2020-05-02 16:46:42 -0700

    Create students.js

---
+++ students.js
@@ -0,0 +1,68 @@
+export const state = () => ({
+  studentObject: [
+    {
+      code: 'VlachM',
+      name: 'Matt Vlach',
+      discipline: 'graphic',
+      tags: ['type-design', 'identity', 'publication', 'installation'],
+      images: 4,
+      ext: ['jpg', 'jpg', 'jpg', 'jpg'],
+      showWork: false,
+      fill: false,
+      text:
+                'Matt Vlach is an Interdisciplinary Designer based in California & practicing across type, book, installation, and identity work. This site is temporary and a new site will be up Summer 2020. Feel free to contact for project inquiries: hi@mattvlach.co'
+    },
+    {
+      code: 'LandsburgR',
+      name: 'Robbie Landsburg',
+      discipline: 'graphic',
+      tags: [
+        'motion design',
+        'branding',
+        '3D Artist',
+        'creative coding',
+        'web design'
+      ],
+      images: 4,
+      ext: ['jpg', 'jpg', 'jpg', 'jpg'],
+      showWork: false,
+      fill: false,
+      text:
+        'I am a visual thinker and I believe in the value of ideas. As a designer, computer scientist, artist, and musician, I love communicating abstract concepts across multiple disciplines, mediums, and artforms. I am not content in rehashing the same solutions to the same problems– instead I celebrate the opportunity to seek new approaches.'
+    },
+    {
+      code: 'OwensM',
+      name: 'Megan Owens',
+      discipline: 'graphic',
+      tags: [
+        'branding',
+        'web design',
+        'UX/UI',
+        'packaging'
+      ],
+      images: 5,
+      ext: ['jpg', 'jpg', 'jpg', 'jpg', 'jpg'],
+      showWork: false,
+      fill: false,
+      text:
+          'I am a graphic designer with a love for craft and positive impacts. I enjoy the process of branding design and finding the tone and look of a brand then implementing it as a system. One of the best things I have experienced with graphic design is the community that comes with it and I’m looking forward to making more connections going forward in my career.'
+    },
+    {
+      code: 'NowackG',
+      name: 'George Nowack',
+      discipline: 'graphic',
+      tags: [
+        'brand strategy',
+        'motion design',
+        'typography',
+        'layout'
+      ],
+      images: 7,
+      ext: ['mp4', 'gif', 'gif', 'png', 'gif', 'gif', 'gif'],
+      showWork: false,
+      fill: false,
+      text:
+            'I am a graphic and motion designer based in California that specializes in brand solutions through design and animation. My passion lies in exploring the psychology of design and how it influences thinking and behavior. I have a focus centered around the incorporation of design within meaningful and intentional brand strategies that effectively connect businesses with their customers.'
+    }
+  ]
+}
